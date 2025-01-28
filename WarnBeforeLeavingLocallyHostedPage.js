65be0b8eb7db53bb836f0c880b7652b377cb2c26
somenewaccountthen <76587846+somenewaccountthen@users.noreply.github.com>
2025-01-28 17:09:28 +0100

    Create WarnBeforeLeavingLocallyHostedPage.js
    Warns when you leave a page that's locally hosted.
    E.g. when you try to drag an image onto a control and you miss the control the page is not replaces by the image and you don'
    t loose all entered values.

---
+++ WarnBeforeLeavingLocallyHostedPage.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Warn leave page Local
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-28
+// @description  try to take over the world!
+// @author       You
+// @match        localhost
+// @match        127.0.0.1
+// @match        0.0.0.0
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=duckduckgo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = 'It looks like you have been editing something. '
+                                + 'If you leave before saving, your changes will be lost.';
+
+        // Modern browsers ignore the custom message and show a generic one
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+})();
