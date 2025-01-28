2e34a3ecd8eb4d545cafc0c921d13c0b5f86bf88
somenewaccountthen <76587846+somenewaccountthen@users.noreply.github.com>
2025-01-28 17:17:02 +0100

    Update TweakersNetNewsFilter_fastest.js
    Use favicon.ico from target site and not google

--- TweakersNetNewsFilter_fastest.js
+++ TweakersNetNewsFilter_fastest.js
@@ -5,7 +5,7 @@
 // @description  Remove unwanted items
 // @author       You
 // @match        https://tweakers.net/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tweakers.net
+// @icon         https://tweakers.net/favicon.ico
 // @grant        none
 // ==/UserScript==
 
